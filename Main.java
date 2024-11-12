482710e5c0d2d31263b6ad7a3244a882fa6fd073
vishnu vardhan reddi <68853334+ugeg2005@users.noreply.github.com>
2024-11-12 10:25:58 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,12 @@
+import java.util.*
+class Parent
+  {
+    Publuc static void main(System[]args)
+    {
+      Scanner sc= new Scanner(System.in)
+      int a=sc.nextInt();
+      int b=sc.nextInt();
+      System.out.println((a+b)+" "+(a-b));
+    }
+  }
+    
